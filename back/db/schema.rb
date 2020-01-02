f4cfe7e2f21d20ded54bcc6b1f4dfa9405e73e16
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-01-02 12:00:00 +0900

    Add new colums to users table.

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_054226) do
+ActiveRecord::Schema.define(version: 2021_01_16_114350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2020_12_07_054226) do
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.text "profile"
   end
 
 end
