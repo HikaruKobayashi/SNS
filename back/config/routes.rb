aec32a1a6744d831bdd50ae6b0c8dd8335a886b2
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-12-07 12:56:02 +0900

    Tweet routes and vue file setting.

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    resources :tweets, only: [:create, :destroy]
+    resources :tweets, only: [:index, :new, :create, :destroy]
     resources :users, only: [:index, :show, :create] do
       member do
         post '/follow', to: 'relationships#create'
---
+++ front/pages/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>ここにツイートを表示</v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
