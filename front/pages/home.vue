636b9ea41b6dbb1cd96b7caaf12507affb6ace03
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-12-08 10:08:33 +0900

    Set up a tweet button.

--- front/pages/home.vue
+++ front/pages/home.vue
@@ -1,15 +1,27 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>ここにツイートを表示</v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>Tweet</v-card-title>
+          <v-card-text>
+            <v-form>
+              <Tweet />
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import Tweet from '~/pages/tweets/tweets.vue'
 export default {
+  components: {
+    Tweet,
+  },
 }
 </script>
-
 <style>
 </style>
