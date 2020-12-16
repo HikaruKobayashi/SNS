edf2e22df17e8d97978569d68d303331c800d8ea
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-12-16 12:00:00 +0900

    Make the logout function a component.

---
+++ front/components/Logout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="currentUser && Number(paramsUserId) == currentUser.user.id">
+    <v-btn
+      @click="logOut"
+      :disabled="disabled"
+    >
+      <span>ログアウト</span>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import firebase from "@/plugins/firebase";
+export default {
+  data () {
+    return {
+      disabled: false,
+    }
+  },
+  computed: {
+    currentUser () {
+      return this.$store.state.currentUser
+    },
+    paramsUserId () {
+      return this.$route.params.id
+    }
+  },
+  methods: {
+    logOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$store.commit("setUser", null);
+          this.$router.push("/login");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
