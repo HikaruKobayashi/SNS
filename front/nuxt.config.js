993704cc49c5a996835c8fab2addf98befb5be78
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-12-26 12:00:00 +0900

    Add new packages.

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -28,6 +28,7 @@ export default {
     "@/plugins/auth-check",
     "@/plugins/vee-validate",
     { src: '~/plugins/aos', ssr: false },
+    { src: '~/plugins/actioncable', ssr: false },
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
