cdfd2e36246549d14d2c8af5a0c499515615e9f1
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2021-02-11 22:25:10 +0900

    Add blog like model.

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_115600) do
+ActiveRecord::Schema.define(version: 2021_02_11_132249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2021_02_08_115600) do
     t.integer "user_id"
     t.integer "blog_id"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blog_likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "blog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
